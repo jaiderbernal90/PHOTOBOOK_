7b83eea9f2743823f6704d4ff9f26de9951e747a
jaiderbernal90 <jaiderbernal90@gmail.com>
2021-07-01 17:04:23 -0500

    REST API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const path = require('path');
 
 // Conectar mongo
 mongoose.Promise = global.Promise;
@@ -14,6 +15,9 @@ const app = express();
 // habilitar el bodyparser
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// función middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas de la app
 app.use('/', routes());
